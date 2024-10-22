37f4c747cfc9ec93cd10e51482e78bccde3d6e5b
nethsarabas <nethsarabas.21@uom.lk>
2024-10-22 09:16:30 +0530

    initial commit

---
+++ src/main/java/com/example/soloway_backend/SolowayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.soloway_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolowayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolowayBackendApplication.class, args);
+	}
+
+}
